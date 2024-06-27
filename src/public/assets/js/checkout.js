7ec6a12174cf0b4a01d911fad16694cbf0fc552f
AnaRenchen <anamagbh@gmail.com>
2024-06-27 18:55:03 -0300

    subiendo

--- src/public/assets/js/checkout.js
+++ src/public/assets/js/checkout.js
@@ -2,6 +2,21 @@ async function checkout() {
   const cid = document.getElementById("cartId").value;
 
   try {
+    Swal.fire({
+      text: "Processing your order...",
+      icon: "info",
+      background: "#87a7ae",
+      color: "black",
+      showConfirmButton: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false,
+      toast: true,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+
     const response = await fetch(`/api/carts/${cid}/purchase`, {
       method: "POST",
       headers: {
@@ -11,9 +26,11 @@ async function checkout() {
 
     const result = await response.json();
 
+    Swal.close();
+
     if (response.ok) {
       if (result.redirect) {
-        Swal.fire({
+        await Swal.fire({
           text: result.message,
           confirmButtonColor: "black",
           imageUrl: "https://i.postimg.cc/rwx3gPhz/icons8-sad-cat-100.png",
