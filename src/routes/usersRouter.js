963648e2bda25faadd2a86ea57b81e37645468a1
AnaRenchen <anamagbh@gmail.com>
2024-07-23 18:59:31 -0300

    ruta premium con middleware

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UsersController } from "../controller/UsersController.js";
+import { authUser } from "../middleware/authUser.js";
 
 export const router7 = Router();
 
-router7.put("/premium/:uid", UsersController.getPremium);
+router7.put("/premium/:uid", authUser(["admin"]), UsersController.getPremium);
